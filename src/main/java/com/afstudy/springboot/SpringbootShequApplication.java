dde8f95a8001b5eae1a86c570025d7daa7661f99
yunzhongjuna <1912292067@qq.com>
2020-02-08 15:22:44 +0800

    init

---
+++ src/main/java/com/afstudy/springboot/SpringbootShequApplication.java
@@ -0,0 +1,13 @@
+package com.afstudy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShequApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShequApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/afstudy/springboot/SpringbootShequApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afstudy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShequApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
